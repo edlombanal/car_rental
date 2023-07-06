d569c135d04136ce546d26c3a486fac6b84e32dd
edlombanal <edlombanal@gmail.com>
2023-07-05 21:27:41 -0500

    Remove stack element from errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,8 +2,7 @@ const errorHandler = (err, req, res, next) => {
     //console.error(err.stack);
     res.status(500).json({
         path: req.path,
-        message: err.message,
-        stack: err.stack
+        message: err.message
     });
 };
 
