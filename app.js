172b89e70f42561111b3c31b7e6853277c828100
edlombanal <edlombanal@gmail.com>
2023-07-05 20:58:10 -0500

    Set up routers in app.js

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+
+//set up routers
+const carController = require('./controllers/carController');
+const reservationController = require('./controllers/reservationController');
+const userController = require('./controllers/userController');
