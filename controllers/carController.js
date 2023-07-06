dea5956a7dbee0f3b9141db16dee52aaf3fb30ff
edlombanal <edlombanal@gmail.com>
2023-07-05 20:33:53 -0500

    Add controller files and carController routers

---
+++ controllers/carController.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+//router for cars
+router.get('/cars', async (req, res, next) => {
+    try {
+        //res.status(200).send("Not implemented.");
+        throw new Error("Not implemented");
+    } catch (error) {
+      next(error);  
+    };
+});
+
+//router for cars by id
+router.get('/cars/:id', async(req, res, next)=>{
+    try {
+        //res.status(200).send("Not implemented.");
+        throw new Error("Not implemented");
+    } catch (error) {
+      next(error);  
+    };
+});
+
+//router to add cars
+router.post('/cars', async(req, res, next)=>{
+    try {
+        //res.status(200).send("Not implemented.");
+        throw new Error("Not implemented");
+    } catch (error) {
+      next(error);  
+    };
+});
+
+//router to deactivate cars
+router.delete('/cars', async(req, res, next)=>{
+    try {
+        //res.status(200).send("Not implemented.");
+        throw new Error("Not implemented");
+    } catch (error) {
+      next(error);  
+    };
+});
