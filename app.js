d4b339200beb5ce6148cd3753d5ab4b2ee0bad24
edlombanal <edlombanal@gmail.com>
2023-07-11 20:55:52 -0500

    Finish register users connection

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 //load routers in the backend
-const carRouter = require('./controllers/carController');
-const reservationRouter = require('./controllers/reservationController');
-const userRouter = require('./controllers/userController');
+const carRouter = require("./controllers/carController");
+const reservationRouter = require("./controllers/reservationController");
+const userRouter = require("./controllers/userController");
 
 //load middlewares in the backend
-const errorHandler = require('./middlewares/errorHandler');
-const logRequest = require('./middlewares/logRequest');
+const errorHandler = require("./middlewares/errorHandler");
+const logRequest = require("./middlewares/logRequest");
 
 //set up middleware as preprocess for requests
 app.use(logRequest);
+app.use(express.json());
 
 //welcome message
-app.get('/', async (req, res)=>{
-    res.send("Welcome to the Car Rental backend's API.")
+app.get("/", async (req, res) => {
+  res.send("Welcome to the Car Rental backend's API.");
 });
 
 //set up routers in app
-app.use('/api/v1', carRouter);
-app.use('/api/v1', reservationRouter);
-app.use('/api/v1', userRouter);
+app.use("/api/v1", carRouter);
+app.use("/api/v1", reservationRouter);
+app.use("/api/v1", userRouter);
 
 //set up middlewares in app
 app.use(errorHandler);
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const app = require('./app');
+const app = require("./app");
+const db = require("./db/client");
 const port = 3001;
 
 const main = async () => {
-    app.listen(port, ()=> {
-        console.log("Backend listening in port " + port);
-    });
+  await db.connectToMongoDB();
+  app.listen(port, () => {
+    console.log("Backend listening in port " + port);
+  });
 };
 
 main();
