ea4c81e21ef7a34e047aa7f21249516d8d48dbdc
edlombanal <edlombanal@gmail.com>
2023-07-12 18:55:09 -0500

    Set up access.log mw

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const fs = require("fs"); //already installed in node
+const statusMonitor = require("express-status-monitor")();
+const xss = require("xss");
 const app = express();
 
 //load routers in the backend
@@ -12,16 +14,28 @@ const userRouter = require("./controllers/userController");
 
 //load middlewares in the backend
 const errorHandler = require("./middlewares/errorHandler");
+const authGuard = require("./middlewares/authGuard");
 
 //set up middleware as preprocess for requests
 app.use(express.json()); //processes request bodies as json
 
-//set up morgan module
+//set up morgan module to keep track of requests in log
 const accessLogStream = fs.createWriteStream("./logs/access.log", {
   flags: "a",
 });
 app.use(morgan("combined", { stream: accessLogStream })); //writes in file
 app.use(morgan("tiny")); //writes in console
+
+//set up status monitor middleware
+app.get("/status", authGuard, statusMonitor.pageRoute);
+app.use(statusMonitor);
+
+//set up welcome message in root route and test xss
+app.get("/", async (req, res) => {
+  res.send(
+    xss("<h1>Welcome to the Backend API</h1><script>alert('hello')</script>")
+  );
+});
 
 //welcome message
 app.get("/", async (req, res) => {
